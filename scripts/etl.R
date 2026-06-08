03b3a0ed625d182687f220d5522e88b11f662b4a
efejiroe <efejiroe@gmail.com>
2026-06-08 13:23:29 +0100

    New commit.

--- scripts/etl.R
+++ scripts/etl.R
@@ -10,12 +10,20 @@ tic('ETL Process complete')
 # Step 1: Extract data from shared location
 if(DataRefresh){
   message('DataRefresh set to "T": Fresh data pull.')
-  FILENAME <- "Healthy Hearts Evaluation Report 1 - patient level data.csv"
-  PATH <- paste0(USERPROFILE, LOCATION, FILENAME)
-  extract <- read.csv(PATH, skip = 9) # Remove rows not needed
+  # Access SharePoint
+  site <- get_sharepoint_site(
+    site_url = "https://emckclac.sharepoint.com/sites/LSMhini",
+    tenant = "emckclac.onmicrosoft.com"
+  )
+  
+  drv <- site$get_drive("Informatics sensitive data") # Get drive
+  drv$list_files(path = "Healthy Hearts") # list files in Healthy Heart folder
+  file_item <- drv$get_item("Healthy Hearts/Healthy Hearts Evaluation Report 1 - patient level data.csv")
+  
+  extract <- file_item$load_dataframe()
   
   # CLEANING
-  # Headers
+  extract <- tail(extract, nrow(extract)-8) # Skip top rows
   extract <- clean_names(extract) # Ensure clean headers
   
   # Column selection
@@ -27,7 +35,7 @@ if(DataRefresh){
     "ethnic_origin"
   )
   
-  dt <- select(extract, paste0(use_cols))
+  dt <- select(extract, paste0(use_cols)) # Use columns
   
   saveRDS(dt, paste0('data/wd_',format(Sys.Date(), "%Y%m%d"))) # Save Working data set
 }else{
--- scripts/ini.R
+++ scripts/ini.R
@@ -1,15 +1,14 @@
 # SYSTEM VARIABLES ----
-# Get user profile
-USERPROFILE <- Sys.getenv("USERPROFILE")
-LOCATION <- "\\King's College London\\HIN - Informatics - Informatics sensitive data\\Healthy Hearts\\"
 
 # LIBRARIES ----
 if (!require("pacman")){
   install.packages("pacman")}
 pacman::p_load(
   'janitor'
-  , 'tidyverse'
-  , 'data.table'
+  ,'tidyverse'
+  ,'data.table'
   ,'skimr' 
   ,'tictoc'
+  ,'Microsoft365R'
 )
+
