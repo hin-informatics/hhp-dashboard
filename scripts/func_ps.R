b4691c94350c17ae482ae0273ffe709fc7a63dcf
efejiroe <efejiroe@gmail.com>
2026-06-08 13:26:04 +0100

    New commit.

--- scripts/func_ps.R
+++ scripts/func_ps.R
@@ -1,6 +1,12 @@
 # HHP TRANSFORMATION FUNCTIONS
 # polly.sinclar@nhs.net
 
+# Instructions:
+# 1. Git Pull
+# 2. Make adjustments between start and end code blocks
+# 3. Test it works
+# 4. Add, Commit and push back to Git.
+
 # Age band Function ----
 # Creates 10-year age band column with data and variable input.
 
