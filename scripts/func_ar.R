7e2c1b1bbeacad3ff68aa8308fbcc9a4505c7e48
Alan Rajan <k2582493@kcl.ac.uk>
2026-06-26 11:46:23 +0100

    New commit

--- scripts/func_ar.R
+++ scripts/func_ar.R
@@ -57,6 +57,34 @@ create_geo_grps <- function(data){
   return(d)
 }
 
+# # Hypertension Optimisation ----
+# 
+# # Patient optimised if diagnosed with Hypertension AND:
+# # ACR < 70 & Systolic < 140 & diastolic < 90
+# # ACR >= 70 & Systolic < 130 & diastolic < 80
+# # ACR value & Frail & Systolic < 150 & diastolic < 90  
+# optimised_htn <- function(data){
+#   message('Creating optimisation flag: Hypertension ')
+#   
+#   d = data
+#   
+#   ## Start code here ##
+#   d <- d %>%
+#     mutate(
+#       hypertension_optimised = case_when(
+#         !is.na(hypertension_diagnosis_code_term) & acr_value < 70 & home_systolic_bp_value < 140 & home_diastolic_bp_value < 90 |
+#           !is.na(hypertension_diagnosis_code_term) & acr_value >= 70 & home_systolic_bp_value < 130 & home_diastolic_bp_value < 80 |
+#           !is.na(hypertension_diagnosis_code_term) & !is.na(acr_value) & !is.na(moderate_or_severe_frailty_code_term) & home_systolic_bp_value < 150 & home_diastolic_bp_value < 90
+#         ~ '1'
+#       ),
+#       hypertension_exist = ifelse(is.na(hypertension_diagnosis_code_term), 'no','yes')
+#     )
+# 
+#   ## End code here ##
+#   
+#   return(d)
+# }
+
 # Hypertension Optimisation ----
 
 # Patient optimised if diagnosed with Hypertension AND:
@@ -72,18 +100,19 @@ optimised_htn <- function(data){
   d <- d %>%
     mutate(
       hypertension_optimised = case_when(
-        !is.na(hypertension_diagnosis_code_term) & acr_value < 70 & home_systolic_bp_value < 140 & home_diastolic_bp_value < 90 |
-          !is.na(hypertension_diagnosis_code_term) & acr_value >= 70 & home_systolic_bp_value < 130 & home_diastolic_bp_value < 80 |
-          !is.na(hypertension_diagnosis_code_term) & !is.na(acr_value) & !is.na(moderate_or_severe_frailty_code_term) & home_systolic_bp_value < 150 & home_diastolic_bp_value < 90
+        !is.na(hypertension_diagnosis_code_term) & acr_value < 70 & clinic_bp_value < 140 & clinic_bp_secondary_value < 90 |
+          !is.na(hypertension_diagnosis_code_term) & acr_value >= 70 & clinic_bp_value < 130 & clinic_bp_secondary_value < 80 |
+          !is.na(hypertension_diagnosis_code_term) & !is.na(acr_value) & !is.na(moderate_or_severe_frailty_code_term) & clinic_bp_value < 150 & clinic_bp_secondary_value < 90
         ~ '1'
       ),
       hypertension_exist = ifelse(is.na(hypertension_diagnosis_code_term), 'no','yes')
     )
-
+  
   ## End code here ##
   
   return(d)
 }
+
 
 
 # Chronic Kidney Disease Optimisation ----
