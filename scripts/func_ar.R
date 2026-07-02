eeaa4c6ce0f0d0ad331d9ede5f19ec92b81de134
efejiroe <efejiroe@gmail.com>
2026-07-02 16:18:29 +0100

    New commit

--- scripts/func_ar.R
+++ scripts/func_ar.R
@@ -63,6 +63,7 @@ create_geo_grps <- function(data){
 # ACR < 70 & Systolic < 140 & diastolic < 90
 # ACR >= 70 & Systolic < 130 & diastolic < 80
 # ACR value & Frail & Systolic < 150 & diastolic < 90 
+
 optimised_htn <- function(data){
   message('Creating optimisation flag: Hypertension ')
   
@@ -71,14 +72,14 @@ optimised_htn <- function(data){
   ## Start code here ##
   d <- d %>%
     mutate(
-      hypertension_exist = ifelse(is.na(hypertension_diagnosis_code_term), 0,1)
+      hypertension_exist = ifelse(is.na(hypertension_diagnosis_code_term), 0, 1)
     ) %>%
     mutate(
       hypertension_optimised = case_when(
         hypertension_exist == 1 & (acr_value < 70 | is.na(acr_value)) & clinic_bp_value < 140 & clinic_bp_secondary_value < 90 ~ 1,
         hypertension_exist == 1 & acr_value >= 70 & clinic_bp_value < 130 & clinic_bp_secondary_value < 80 ~ 1,
         hypertension_exist == 1 & !is.na(acr_value) & !is.na(moderate_or_severe_frailty_code_term) & clinic_bp_value < 150 & clinic_bp_secondary_value < 90 ~ 1,
-        TRUE ~ 0)
+        hypertension_exist == 0 ~ 0)
     )
   ## End code here ##
   
@@ -88,9 +89,8 @@ optimised_htn <- function(data){
 # Chronic Kidney Disease Optimisation ----
 
 # Patient optimised if diagnosed with CKD AND:
-  # Has got (Diabetes AND acr >=3 OR has got no diabetes AND acr >=22.6) AND is taking any dose of (acei OR arb AND sglt2i AND Statin, the medication should be CURRENT) -- Where is intolerance?
-  # Any AND Statin
-
+# YES Diabetes AND ACR >= 3 & taking ACEi OR ARB + SGLT2i + statin 
+# NO Diabetes AND ACR >= 22.6 & & taking ACEi OR ARB + SGLT2i + statin
 
 optimised_ckd <- function(data){
   message('Creating optimisation flag: CKD ')
@@ -100,7 +100,7 @@ optimised_ckd <- function(data){
   ## Start code here ##
   d <- d %>% 
     mutate(
-      ckd_exists = ifelse(is.na(ckd_diagnosis_code_term), 0,1)
+      ckd_exists = ifelse(is.na(ckd_diagnosis_code_term), 0, 1)
     ) %>%
     mutate(
       ckd_optimised = case_when(
@@ -117,9 +117,9 @@ optimised_ckd <- function(data){
 
 # Diabetes ----
 
-# Patient optimised if diagnosed with Diabetes AND:
-# no frailty value AND metformin AND sglt2i AND hba1c <= 53
-# frailty has value which is Moderate or Severe AND metformin AND hba1c <= 75
+# Patient optimised if diagnosed with Type 2 Diabetes AND:
+# NO frailty (blank) & taking Metformin + SGLT2i & HbA1c <= 53
+# YES frailty & taking Metformin & HbA1c <= 75
 
 optimised_t2d <- function(data){
   message('Creating optimisation flag: Type 2 diabetes ')
@@ -144,10 +144,12 @@ optimised_t2d <- function(data){
 
 # All patients  ----
 
-# Patient optimised if:
-# CVD: Has value in (chd AND pad AND pvd AND (non_hemorrhagic_stroke OR tia)) AND (ldl <= 2 OR non_hdl <=2.6)
-# No CVD: Has value in (chd AND pad AND pvd AND (non_hemorrhagic_stroke OR tia)) AND (ldl <= 2 OR non_hdl <=2.6) AND (qrisk >=10) AND (antilipid medication AND ldl <= 2)
-# ALL: Has value in acr AND bp AND hba1c
+# Patient optimised if no HYP, CKD or T2D AND:
+# YES CVD (i.e. CHD, PAD, PVD, Stroke OR TIA = TRUE) & Non-HDL <= 2.6
+# NO CVD AND QRISK >= 10 & on Statin
+
+# Patients are NOT optimised if missing ACR, BP, HbA1Ac records.
+
 
 optimised_all <- function(data){
   message('Creating optimisation flag: All patients ')
